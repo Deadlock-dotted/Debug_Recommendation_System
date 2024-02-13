5c78c0890618f7f70189979adc4b7df3a8617398
suhilroshan <msrrise010@gmail.com>
2024-02-13 23:01:49 +0530

    extractor and keywords python file -- for extracting the inforamtion from the log file

---
+++ keywords.py
@@ -0,0 +1,37 @@
+"""
+Takes document as input and performs chunking accoring to a pattern to return a list of Candidate Keywords.
+
+Input :
+    document - text file
+Output :
+    list of possible keyphrases
+
+"""
+import nltk
+from nltk import word_tokenize
+from nltk.chunk import RegexpParser
+
+
+def extract_candidate_keywords(document):
+
+    #Get the words in the document
+    words = word_tokenize(document)
+    print('The Extracted keywords',  words)
+    # Chunk first to get 'Candidate Keywords'
+    tagged = nltk.pos_tag(words)
+    chunkGram = r""" PHRASE: 
+                        {(<JJ>* <NN.*>+ <IN>)? <JJ>* <NN.*>+}
+                """
+
+    chunkParser = RegexpParser(chunkGram)
+    chunked = chunkParser.parse(tagged)
+
+    print('The Chunked Keywords', chunked)
+    candidate_keywords = []
+    for tree in chunked.subtrees():
+        if tree.label() == 'PHRASE':
+            candidate_keyword = ' '.join([x for x,y in tree.leaves()])
+            candidate_keywords.append(candidate_keyword)
+
+    candidate_keywords = [w for w in candidate_keywords if len(w) > 3 and  len(w.split(' ')) < 6] 
+    return candidate_keywords
